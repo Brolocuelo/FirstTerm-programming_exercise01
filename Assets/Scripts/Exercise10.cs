10da75066938bd997c70067fafd23d3e655f0bcf
Brolocuelo <luisalfonsorello@gmail.com>
2022-11-15 13:31:43 +0100

    Ejercicio terminado

--- Assets/Scripts/Exercise10.cs
+++ Assets/Scripts/Exercise10.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Exercise10 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int x = 2;
+    
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log($"{x} * 1 = {x * 1}");
+        Debug.Log($"{x} * 2  = {x * 2}");
+        Debug.Log($"{x} * 3  = {x * 3}");
+        Debug.Log($"{x} * 4  = {x * 4}");
+        Debug.Log($"{x} * 5  = {x * 5}");
+        Debug.Log($"{x} * 6  = {x * 6}");
+        Debug.Log($"{x} * 7  = {x * 7}");
+        Debug.Log($"{x} * 8  = {x * 8}");
+        Debug.Log($"{x} * 9  = {x * 9}");
+        Debug.Log($"{x} * 10  = {x * 10}");
     }
 }
--- Assets/Scripts/Exercise9.cs
+++ Assets/Scripts/Exercise9.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class Exercise9 : MonoBehaviour
 {
+    public int avaibleCandy;
+    public int amountOfPeople;
+    private int result;
+    private int result2;
 
+    private void Start()
+    {
+        result = avaibleCandy % amountOfPeople;
+        result2 = avaibleCandy / amountOfPeople;
+        Debug.Log(string.Format("Each person takes {0}, and {1} are left",result2,result));
+    }
 }
